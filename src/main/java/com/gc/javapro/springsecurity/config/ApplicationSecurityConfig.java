d105d08ea1ca1e5b6bdbe915c41ed371114efec6
Gaurav Chhabra <gc1133@gmail.com>
2021-11-16 12:35:57 +0530

    final checkin

--- src/main/java/com/gc/javapro/springsecurity/config/ApplicationSecurityConfig.java
+++ src/main/java/com/gc/javapro/springsecurity/config/ApplicationSecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.gc.javapro.springsecurity.config.ApplicationUserPermission.STUDENT_WRITE;
 import static com.gc.javapro.springsecurity.config.ApplicationUserRole.*;
 
@@ -42,7 +44,25 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/login").permitAll()
+                    .defaultSuccessUrl("/courses", true)
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                .and()
+                .rememberMe() // defaults to 2 weeks
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                    .key("somethingverysecured")
+                    .rememberMeParameter("remember-me")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID","remember-me")
+                    .logoutSuccessUrl("/login")
+
+        ;
     }
 
     @Override
